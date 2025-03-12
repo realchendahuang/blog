53ec535d3f9965b91126e5d2c711feea4f9417e8
realchendahuang <realchendahuang@gmail.com>
2025-03-12 10:52:34 +0800

    change config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,14 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://cdhdanke.top/", // replace this with your deployed domain
+  author: "陈大黄",
+  profile: "",
+  desc: "你好，这里是陈大黄的博客。当过AI产品经理，现在自己写代码做独立开发，主业研究怎么用AI高效学习外语（顺便拯救打工人的生产力）。日常爱折腾工具、写段子，从产品设计到写代码再到运营推广全都能掺一脚。这里分享AI避坑指南、语言学习野路子，以及互联网打杂选手的日常思考。",
+  title: "陈大黄的博客",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 6,
+  postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
-  editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
-    text: "Suggest Changes",
-    appendFilePath: true,
-  },
 } as const;
